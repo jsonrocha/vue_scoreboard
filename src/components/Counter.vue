aa7fa411bcb481cb7c878522ed32b6af41285f55
Rocha <jrocha@proforma.com>
2018-09-04 15:38:44 -0400

    Almost There

---
+++ src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>{{count}}</h1>
+        <h2>{{personName}}</h2>
+        <button v-on:click="addOne">Add 1!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Counter",
+    data:function(){
+        return {
+            count:0,
+            personName:"Stan",
+            address:"123 Fake street"
+        }
+    },
+    methods:{
+        addOne:function(){
+            this.count++;
+        },
+        subtractOne:function(){
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        color:purple;
+    }
+</style>
---
+++ src/components/Counter2.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <h1>{{count}}</h1>
+        <h2>{{personName}}</h2>
+        <button v-on:click="addOne">Add 1!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Counter2",
+    data:function(){
+        return {
+            count:0,
+            personName:"Steve",
+            address:"123 Fake street"
+        }
+    },
+    methods:{
+        addOne:function(){
+            this.count++;
+        },
+        subtractOne:function(){
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+        color:purple;
+    }
+</style>
